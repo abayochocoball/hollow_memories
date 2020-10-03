6b1ed68b7c569f8116623d8aae7d2fb24bcf3840
abayochocoball <68942401+abayochocoball@users.noreply.github.com>
2020-10-03 02:53:16 -0700

    add script

--- scripts/postprocess.ps1
+++ scripts/postprocess.ps1
@@ -1,3 +1,12 @@
+<#
+Applys post processing to livestream recordings.
+
+usage:
+postporcess.ps1 <stream_url>
+
+Run this script while the stream is live.
+Press enter after the stream is saved.
+#>
 $stream_url = $args[0]
 $filename = youtube-dl --write-description --skip-download -o "[%(uploader)s][%(upload_date)s] %(title)s (%(id)s)" --get-filename $stream_url
 youtube-dl --write-thumbnail --write-description --skip-download -o stream $stream_url
